ef702a86c7ae16f35a68cb839b8af890445bd0d0
Howiew777 <howiewaldenmeyer@amdg.rockhursths.edu>
2020-03-03 08:51:12 -0600

    ligma

---
+++ adventure game.py
@@ -0,0 +1,6 @@
+print("You are a Jewish man with your family in Nazi Germany hiding. The Gustapo has entered your house and is suspect of your helper.You must escape or there is a chance you and your family may die. What is your first choice?")
+
+
+print("You can attempt to run or try to fight the Gustapo")
+if run == "escape by yourself":
+    if fight == "be tken by Gustapo":
