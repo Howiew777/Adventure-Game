8b133b54e5af0d05a2206181caa92a7e8a51fa5b
Howiew777 <howiewaldenmeyer@amdg.rockhursths.edu>
2020-03-04 08:52:35 -0600

    C

--- adventure game.py
+++ adventure game.py
@@ -1,6 +1,20 @@
-print("You are a Jewish man with your family in Nazi Germany hiding. The Gustapo has entered your house and is suspect of your helper.You must escape or there is a chance you and your family may die. What is your first choice?")
-
-
+print("You are a Jewish man with your family in Nazi Germany hiding. The Gestapo has entered your house and is suspect of your helper. You must escape or there is a chance you and your family may die. What is your first choice?")
 print("You can attempt to run or try to fight the Gustapo")
-if run == "escape by yourself":
-    if fight == "be tken by Gustapo":
+answer = input("your wife asks what to do your options are: Run, fight, Hide")
+if(answer == "run"):
+    print("You escape but leave your family behind")
+    answer1 = input("Would you like to go to the city, country, or trainstation?")
+    elif(answer == "city"):
+    elif(answer == "country"):
+    elif(answer == "trainstation"):    
+    elif(answer == "fight"):
+    print("You beat up the men but your wife is injured")
+        elif(answer == "Hide"):
+    print("You are found and taken by the gestapo, better luck next time")
+
+print()
+
+
+
+
+
